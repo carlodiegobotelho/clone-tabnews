9afc05c4a1f6c15ca68f6a6f3dfdecf4f15369f4
Diego Botelho <72636467+carlodiegobotelho@users.noreply.github.com>
2024-09-13 03:49:25 +0000

    added scripts `lint:check` and `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Amo você ! Você é uma mãe e uma mulher incrível s2 !!</h1>
+  return <h1>Amo você ! Você é uma mãe e uma mulher incrível s2 !!</h1>;
 }
 
 export default Home;
